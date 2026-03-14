a5227d4c3197209beaad367483ed7c665181cebb
jmnu4245 <juanmanuelfloresdelacruz@gmail.com>
2026-03-14 13:32:17 +0000

    _e2 para test automatizados

--- comp/java/testJM.sh
+++ comp/java/testJM.sh
@@ -6,6 +6,9 @@ for i in $2/*.java; do
     #comprobamos si continene _e1 en ese caso lo lanzamos sin -l, solo sirve para meta1
     if [[ "$i" == *_e1* ]]; then
         ./jucompiler < "$i" | diff "${i/%.java}.out" -
+    #de forma similar si contiene _e2 lo lanzamos sin -t, solo sirve para meta2
+    elif [[ "$i" == *_e2* ]]; then
+        ./jucompiler < "$i" | diff "${i/%.java}.out" -
     else
         ./jucompiler $1 < "$i" | diff "${i/%.java}.out" -
     fi
